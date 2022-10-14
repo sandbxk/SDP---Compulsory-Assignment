7338de75e3c042643b63eb698c639c1929705197
sandbxk <90904706+sandbxk@users.noreply.github.com>
2022-10-14 15:08:12 +0200

    DbBuilding research

---
+++ Backend/Infrastructure/DbBuild/Database.cs
@@ -0,0 +1,43 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+
+namespace Infrastructure.DbBuild;
+
+public static class Database
+{
+
+    public static void EnsureDatabase(string connectionString, string name)
+    {
+        var parameters = new DynamicParameters();
+        parameters.Add("name", name);
+        using var connection = new SqliteConnection(connectionString);
+        var records = connection.Query("SELECT * FROM sys.databases WHERE name = @name",
+            parameters);
+        if (!records.Any())
+        {
+            connection.Execute($"CREATE DATABASE {name}");
+        }
+    }
+    
+    public static void RecreateDatabase(string connectionString, string name)
+    {
+        var parameters = new DynamicParameters();
+        parameters.Add("name", name);
+        using var connection = new SqliteConnection(connectionString);
+        connection.Execute($"DROP DATABASE IF EXISTS {name}");
+        connection.Execute($"CREATE DATABASE {name}");
+    }
+    
+    public static bool TableExists(string connectionString, string name)
+    {
+        var parameters = new DynamicParameters();
+        parameters.Add("name", name);
+        using var connection = new SqliteConnection(connectionString);
+        var records = connection.Query("SELECT * FROM sys.tables WHERE name = @name",
+            parameters);
+
+        return records.Any();
+    }
+    
+}
---
+++ Backend/Infrastructure/DbBuild/Migration_1.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Infrastructure.DbBuild;
+
+public class Migration_1 : Migration
+{
+    public override void Up()
+    {
+        Delete.Table("Boxes");
+    }
+
+    public override void Down()
+    {
+        Create.Table("Boxes")
+            .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+            .WithColumn("Content").AsString();
+
+    }
+}
